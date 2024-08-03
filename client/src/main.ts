d4a96ed53fb04abea583bf13318df13d55758fa6
yashweblife <yashweblife20@gmail.com>
2024-08-02 19:43:51 -0500

    add stream

--- client/src/main.ts
+++ client/src/main.ts
@@ -13,13 +13,19 @@ fetch('http://localhost:3300/chat', {
 
 function app() {
   const canvas = document.createElement('canvas') as HTMLCanvasElement;
+  const stream = document.getElementById('stream') as HTMLImageElement;
   const saveButton = document.querySelector('#send-button') as HTMLButtonElement;
+  const streamButton = document.querySelector('#stream-button') as HTMLButtonElement;
   const clearButton = document.querySelector('#clear-button') as HTMLButtonElement;
   const c = canvas.getContext('2d') as CanvasRenderingContext2D;
-  canvas.width = 400;
-  canvas.height = 400;
+  canvas.width = 300;
+  canvas.height = 300;
+  c.beginPath();
+  c.fillStyle = 'white';
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  c.closePath();
   document.body.appendChild(canvas)
-
+  c.fillStyle = 'red';
   const mouse = { x: 0, y: 0, down: false };
   window.addEventListener('mousedown', (e) => {
     mouse.down = true;
@@ -39,7 +45,7 @@ function app() {
   });
 
   saveButton.addEventListener('click', () => {
-    const dataUrl = canvas.toDataURL('image/png');
+    const dataUrl = canvas.toDataURL('image/jpeg');
     const filter = dataUrl.split(',')[1]
     fetch('http://localhost:3300/image', {
       method: 'post',
@@ -49,9 +55,29 @@ function app() {
       body: JSON.stringify({ prompt: filter })
     }).then((res) => res.json()).then((data) => console.log(data))
   });
-
+  streamButton.addEventListener('click', () => {
+    c.drawImage(stream, 0, 0, 200, 200)
+    const dataUrl = canvas.toDataURL('image/jpeg');
+    const final = dataUrl.split(',')[1]
+    fetch('http://localhost:3300/image', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ prompt:final })
+    })
+    .then((res) => res.json()).then((data) => {
+      console.log(data.message.content)
+    })
+    })
   clearButton.addEventListener('click', () => {
     c.clearRect(0, 0, canvas.width, canvas.height);
+    c.beginPath();
+    c.fillStyle = 'white';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    c.closePath();
+    document.body.appendChild(canvas)
+    c.fillStyle = 'red';
   });
 }
 app()
